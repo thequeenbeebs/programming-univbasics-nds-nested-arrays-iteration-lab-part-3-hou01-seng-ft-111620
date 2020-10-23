df48f4ec2bb9bc39b8e690b858b2c0cef129c6df
Blaire Baker <bakerblaire@gmail.com>
2020-10-23 19:10:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while count < src.length do
     inner_count = 0
     while inner_count < src[count].length do
-      if src[count][inner_count].class == String do
+      if src[count][inner_count].class == String
         all_strings += src[count][inner_count]
       end
       inner_count += 1
